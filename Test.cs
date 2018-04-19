151c859e7bb21174a98053e00dd5097d5afad799
AlexanderBerezhniy <alexandr.berezhniy18@gmail.com>
2018-04-19 22:05:05 +0300

    AutoTests

---
+++ Test.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Chrome;
+
+namespace AutoTest
+{
+    [TestFixture]
+    public class Test
+    {
+        [OneTimeSetUp] // вызывается перед началом запуска всех тестов
+        public void OneTimeSetUp()
+        {
+            // ТУТ КОД
+        }
+
+        [OneTimeTearDown] //вызывается после завершения всех тестов
+        public void OneTimeTearDown()
+        {
+            // ТУТ КОД
+        }
+
+        [SetUp] // вызывается перед каждым тестом
+        public void SetUp()
+        {
+            // ТУТ КОД
+        }
+
+        [TearDown] // вызывается после каждого теста
+        public void TearDown()
+        {
+            // ТУТ КОД
+        }
+
+        [Test]
+        public void TEST_1()
+        {
+            // ТУТ КОД
+        }
+
+        [Test]
+        public void TEST_2()
+        {
+            // ТУТ КОД
+        }
+    }
+}
